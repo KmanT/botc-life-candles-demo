7a84cf53c854e020fdc55c43343e87ab73bf1b78
Kyle Turske <kyle.turske@merkle.com>
2024-12-20 15:43:44 -0500

    fix(src/components/CandleCount.jsx): prevent page reload onSubmit

--- src/components/CandleCount.jsx
+++ src/components/CandleCount.jsx
@@ -3,7 +3,13 @@ import { useRef } from 'react';
 export default function CandleCount ({ onCountChange }) {
     const count = useRef()
 
-    return <form id='candle-form' className='card' onSubmit={() => onCountChange(count.current.value)}>
+    function handleSubmit (event) {
+        event.preventDefault();
+
+        onCountChange(count.current.value);
+    }
+
+    return <form id='candle-form' className='card' onSubmit={handleSubmit}>
         <label htmlFor="candle-count">Number of candles</label>
         <input ref={count} id="candle-count" type="number" />
         <button >Set Candle Count</button>
